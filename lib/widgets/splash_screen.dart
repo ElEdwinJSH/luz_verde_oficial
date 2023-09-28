839340a41d54fcbe79c88e94a44ce29437eaea4c
Edwin Jesus Santillan Hernandez <deadwiner69@gmail.com>
2023-09-27 21:39:22 -0600

    Splash Screen centrado

--- lib/widgets/splash_screen.dart
+++ lib/widgets/splash_screen.dart
@@ -15,12 +15,13 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3), () {
+    Timer(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => HomeScreen()));
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
     });
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
